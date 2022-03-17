11764cd9dec90f76bbb091e98967e9e0d5969b4a
ysfei <ysfei92@gmail.com>
2022-03-17 16:20:42 -0500

    Update 121. Best Time to Buy and Sell Stock.py

--- sort_by_leetcode/array/easy/121. Best Time to Buy and Sell Stock.py
+++ sort_by_leetcode/array/easy/121. Best Time to Buy and Sell Stock.py
@@ -24,8 +24,12 @@ class Solution(object):
         :type prices: List[int]
         :rtype: int
         """
-        curSum=maxSum=0
+        curSum=maxSum=0 #可以连等，不错！
         for i in range(1,len(prices)):
-            curSum=max(0,curSum+prices[i]-prices[i-1])
+            curSum=max(0,curSum+prices[i]-prices[i-1]) 
+    #开始的条件是当前price比上一个起始点更低，即从上一个起始点开始累计的return<0，此时重新开始累计
             maxSum = max(curSum,maxSum)
+    #结束的条件是当前的累计return比储存的所有累计return都要高
         return maxSum
+    #这一类题目（对一个动作的起始点都要做决定）都可以通过在每个点做决定的方法来减少时间复杂度：每个点决定要不要开始/结束
+    #节省时间复杂度，暴力解法是对起始点各做循环，复杂度O(n^2)。现在复杂度是O(n)
