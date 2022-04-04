c075362ee9847793eca25704aa236071988d72cf
ysfei <ysfei92@gmail.com>
2022-04-04 02:48:17 -0500

    Update 169. Majority Element.py

--- sort_by_leetcode/array/easy/169. Majority Element.py
+++ sort_by_leetcode/array/easy/169. Majority Element.py
@@ -14,7 +14,18 @@ Subscribe to see which companies asked this question.
 """思路，因为出现次数最多的元素大于元素个数的一半，所以使用count和cand，设目标值为a
 """
 
-
+"""
+暴力解法：
+class Solution(object):
+    def majorityElement(self, nums):
+        majority_count = len(nums)//2
+        for num in nums:
+            count = sum(1 for elem in nums if elem == num)
+            if count > majority_count:
+                return num
+这里可以学习对list里出现某个值的计数方法：count = sum(1 for i in nums if i==target)
+但这个解法暴力在sum(1 for ...)本身也是循环，所以复杂度是n^2.
+                
 class Solution(object):
     def majorityElement(self, nums):
         """
