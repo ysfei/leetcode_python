5988c9545d8ab0a8e51503aca07af012cfa040ec
ysfei <ysfei92@gmail.com>
2022-04-05 16:38:30 -0500

    Update 169. Majority Element.py

--- sort_by_leetcode/array/easy/169. Majority Element.py
+++ sort_by_leetcode/array/easy/169. Majority Element.py
@@ -11,9 +11,6 @@ Subscribe to see which companies asked this question.
 
 """
 
-"""思路，因为出现次数最多的元素大于元素个数的一半，所以使用count和cand，设目标值为a
-"""
-
 """
 暴力解法(n^2)：
 class Solution(object):
@@ -26,6 +23,14 @@ class Solution(object):
 这里可以学习对list里出现某个值的计数方法：count = sum(1 for i in nums if i==target)
 但这个解法暴力在sum(1 for ...)本身也是循环，所以复杂度是n^2.
 
+巧思路（NlogN)
+class Solution:
+    def majorityElement(self, nums):
+        nums.sort()
+        return nums[len(nums)//2]
+        #或者直接一行：return sorted(nums)[len(nums)//2]
+缺点是sort的时间复杂度是NlogN
+
 Hash table解法(n):
 from collections import Counter
 class Solution:
@@ -33,30 +38,21 @@ class Solution:
         counts = Counter(nums)
         return max(counts.keys(), key=counts.get)
 这里import了Counter函数，直接生成各个值的频度表，format是dictionary (Hash table)
-max函数。。。
-                
+max函数的key选项用于比大小的数字和return数字不同的情况，比如要比较x^2时就用 max(nums, key=lambda x: x**2)
+counts.keys()表示要return的数字是ditionary keys种的一个（得有括号，否则只是个函数）。counts.get是个函数（没有括号），给出key对应的值
+也可以把函数用lambda写出来 max(counts.keys(), key=lambda x:counts[x])
+Counter和max都是n时间复杂度，所以总共时间复杂度也是n。
+
+Boyer-Moore Voting Algorithm （n)
 class Solution(object):
     def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        cand = nums[0]
-        count = 1
-        for i in nums[1:]:
-            if count == 0:
-                cand, count = i, 1
+        candidate, count = nums[0], 0
+        for num in nums:
+            if num == candidate:
+                count += 1
+            elif count == 0:
+                candidate, count = num, 1
             else:
-                if i == cand:
-                    count = count + 1
-                else:
-                    count = count - 1
-        return cand
-"""一行的思路"""
-class Solution(object):
-    def majorityElement(self, nums):
-        """
-        :type nums: List[int]
-        :rtype: int
-        """
-        return sorted(nums)[len(nums)/2]
+                count -= 1
+        return candidate
+
