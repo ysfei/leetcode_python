29b800b9eeba5e386b2307b629f5f483b134044a
ysfei <ysfei92@gmail.com>
2022-04-07 17:52:20 -0500

    Update 27. Remove Element.py

--- sort_by_leetcode/array/easy/27. Remove Element.py
+++ sort_by_leetcode/array/easy/27. Remove Element.py
@@ -26,6 +26,20 @@ class Solution(object):
         :rtype: int
         """
         index = 0
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[index] = nums[i]
+
+
+
+class Solution(object):
+    def removeElement(self, nums, val):
+        """
+        :type nums: List[int]
+        :type val: int
+        :rtype: int
+        """
+        index = 0
         for i in range(0,len(nums)):
             if nums[i] != val:
                 nums[index] = nums[i]
