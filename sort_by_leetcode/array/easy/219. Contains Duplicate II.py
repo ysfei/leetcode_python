61b7b3814af18f83738c5fceb0ebd8ecd0a9e0cb
ysfei <ysfei92@gmail.com>
2022-04-07 15:48:51 -0500

    Update 219. Contains Duplicate II.py

--- sort_by_leetcode/array/easy/219. Contains Duplicate II.py
+++ sort_by_leetcode/array/easy/219. Contains Duplicate II.py
@@ -4,8 +4,9 @@ Given an array of integers and an integer k, find out whether there are two dist
 
 """
 """仍然用dict保存数组元素出现的位置，两种情况下更新"""
-#和217一样，无非在判断num in dic时多加一个条件：index - dic[num] <= k
+#和217一样，无非在判断num in dic时多加一个条件：index - dic[num] <= k，以及在dict中的value不能乱写了，得用nums的index
 #for i in nums: 返回list values
+#for i,value in enumerate(nums): 返回nums的 (index,value) pair
 #for i in dic: 返回dictionary keys, 相当于for i in dic.keys()
 #for i in dic.values(): 返回dictionary values
 #for i,value in dic.items(): 返回dictionary的 (key,value) pair
