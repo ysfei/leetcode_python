db7ad25720a64b6f3fc9205b340bd4f20faff9f6
ysfei <ysfei92@gmail.com>
2022-04-07 15:45:44 -0500

    Update 189. Rotate Array.py

--- sort_by_leetcode/array/easy/189. Rotate Array.py
+++ sort_by_leetcode/array/easy/189. Rotate Array.py
@@ -19,7 +19,11 @@ Subscribe to see which companies asked this question.
 """
 """直接拼接数组即可"""
 
-#就是左边变成n-k+1到n，右边变成1到n-k. 注意slicing是左开右闭，所以写成nums[n-k:n]和nums[0:n-k].而0和n是起始index所以可以省略
+#就是左边变成n-k+1到n，右边变成1到n-k. 
+#注意slicing是左闭右开，同时index=position-1。所以写成nums[n-k:n]和nums[0:n-k].而0和n是起始index所以可以省略
+#rule of thumb 1: 如果想要slice positions [a,b], 写成nums[a-1:b], 里面个数是b-a+1.如果a-1=0或b=N可以省略，其中N是指len(nums)
+#rule of thumb 2: 反过来，code中的nums[a:b]指的是positions[a+1,b], 里面个数是b-a.如果a省略了说明a=0，那就是positions[1,b],如果b省略了说明b=N，那就是[a+1,N]
+#注意nums[N]不存在，但是nums[N-1:N]就是最后一个元素nums[N-1]
 #不知道为什么LHS写成nums[:]，直接用nums就可以了，别忘了还要写return nums
 class Solution(object):
     def rotate(self, nums, k):
