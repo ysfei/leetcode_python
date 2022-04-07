5594d75b73c66b1fcccc9af32e6c0c6b76242776
ysfei <ysfei92@gmail.com>
2022-04-07 15:03:57 -0500

    Update 217. Contains Duplicate.py

--- sort_by_leetcode/array/easy/217. Contains Duplicate.py
+++ sort_by_leetcode/array/easy/217. Contains Duplicate.py
@@ -6,7 +6,8 @@ Subscribe to see which companies asked this question.
 
 """
 """利用hash table，别忘了判断空数组的情况.其实更麻烦"""
-
+#如果是暴力解法，会在loop over num的过程中再与过去的出现过的num进行比较，O(N^2).
+#所以常用技巧就是把查找过去出现过的num的任务交给dict，num in dict的复杂度只O(1).所以总的复杂度只有O(N)
 class Solution(object):
     def containsDuplicate(self, nums):
         """
@@ -15,15 +16,15 @@ class Solution(object):
         """
         if not nums:
             return False
-        dic = dict()
+        dic = dict() #建立空dict的方法： dict()
         for num in nums:
             if num in dic:
-                return True
-            dic[num] = 1
+                return True #一旦出现return，整个def直接结束（不只是loop结束）
+            dic[num] = 1 #if num in dic的in函数判断的是keys而不是dic中的values，所以直接用当前num作为key，value就随便（比如1）
         return False
 
 """直接利用set的性质反而更简单"""
-
+#set()提取nums中的distinct values生成一个set. 原因很简单：set这种data type不能包含重复元素
 class Solution(object):
     def containsDuplicate(self, nums):
         """
